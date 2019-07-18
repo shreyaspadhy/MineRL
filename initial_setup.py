2e4ac074cd565cc0b94810f669711453b4ba3c20
Haroun Habeeb <haroun7@fb.com>
2019-07-18 00:45:37 -0700

    Getting ready to write up a simple baseline to get the ball rolling

---
+++ initial_setup.py
@@ -0,0 +1,16 @@
+'''
+Just a bunch of commands that you should run manually
+Don't have time to automate this.
+'''
+
+# 1: Install JDK-8
+# 2: Install mineRL
+pip3 install --upgrade minerl
+
+# 3: Installing RLLib
+pip install tensorflow  # or tensorflow-gpu
+pip install ray[rllib]
+pip install ray[debug]
+
+git clone https://github.com/ray-project/ray
+cd ray/python/ray/rllib
