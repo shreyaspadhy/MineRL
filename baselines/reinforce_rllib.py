e2f14975f77b77903b3b065931cab72e9dd18d1e
Shreyas Padhy <shreyas@Shreyass-MacBook-Pro.local>
2019-07-18 23:08:52 -0400

    Wrapped MineRL env in RLLib custom env

--- baselines/reinforce_rllib.py
+++ baselines/reinforce_rllib.py
@@ -2,6 +2,8 @@ import gym
 import ray
 from ray.rllib.agents import ppo
 
+# Template of custom environment
+
 
 class MyEnv(gym.Env):
     def __init__(self, env_config):
@@ -13,6 +15,29 @@ class MyEnv(gym.Env):
 
     def step(self, action):
         return < obs > , < reward: float > , < done: bool > , < info: dict >
+
+
+# Wrapping the MineRL Environment
+class MineRLEnv(gym.Env):
+    def __init__(self, env_config):
+        """MineRL Environment for use
+
+        env_config is a dict containing all parameters to pass thru trainer
+        """
+        # Is there a way to access the obs and action space without making the
+        # env, needing MC to start up and take hella time
+        self.environ = gym.make(env_config.name)
+
+        self.action_space = self.environ.action_space
+        self.observation_space = self.environ.observation_space
+
+    def reset(self):
+        return self.environ.reset()
+
+    def step(self, action):
+        obs, rew, done, info = self.environ.step(action)
+
+        return obs, rew, done, info
 
 
 ray.init()
