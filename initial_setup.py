947131b08390025d53b1a01c76e3f8855c490541
Shreyas Padhy <shreyas@Shreyass-MacBook-Pro.local>
2019-07-18 22:56:18 -0400

    First trials with RLLib, install+hello world

--- initial_setup.py
+++ initial_setup.py
@@ -5,12 +5,14 @@ Don't have time to automate this.
 
 # 1: Install JDK-8
 # 2: Install mineRL
-pip3 install --upgrade minerl
+pip3 install - -upgrade minerl
 
 # 3: Installing RLLib
-pip install tensorflow  # or tensorflow-gpu
-pip install ray[rllib]
-pip install ray[debug]
+pip install tensorflow == 2.0.0 - beta1  # or tensorflow-gpu
+pip install ray  # This works by itself
+pip install lz4  # RLLib PPO needs this
+# pip install ray[rllib]
+# pip install ray[debug]
 
-git clone https://github.com/ray-project/ray
-cd ray/python/ray/rllib
+git clone https: // github.com / ray - project / ray
+cd ray / python / ray / rllib
